c615152b516d28b38cb22c036408867b8cf10fad
Tahir Iqbal Najam <tahiriqbal09@gmail.com>
2025-05-20 02:33:11 +0500

    remove history

--- .history/administrator/components/com_zakeke/sql/install.mysql.utf8_20250103013919.sql
+++
@@ -1,7 +0,0 @@
--- administrator/components/com_zakeke/sql/install.mysql.utf8.sql
-CREATE TABLE IF NOT EXISTS `#__zakeke_config` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `client_id` varchar(255) NOT NULL,
-  `client_secret` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
